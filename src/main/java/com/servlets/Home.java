18ee3edec8ec49f4f9c092347cfcf9901e3f60b4
jw406 <jwvic.2357@gmail.com>
2021-06-21 19:37:21 -0400

    track visitor ip

--- src/main/java/com/servlets/Home.java
+++ src/main/java/com/servlets/Home.java
@@ -4,12 +4,17 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 // import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.utils.DBConnection;
 
 public class Home extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -21,13 +26,20 @@ public class Home extends HttpServlet {
 
     try (FileInputStream fis = new FileInputStream(getClass().getClassLoader().getResource(file).getFile());
         BufferedInputStream bis = new BufferedInputStream(fis)) {
+      // log user info to DB;
+      logToDB(request.getRemoteHost());
+
+      // return content to client
       response.setContentType("text/html");
       out.print("<div>hello</div>");
       out.println("<br/>");
+
+      // read in the file, append the bytes to StringBuilder
       StringBuilder sb = new StringBuilder();
       while (bis.available() > 0) {
         sb.append((char) bis.read());
       }
+      // display file content
       out.println("<div>" + sb.toString() + "</div>");
     } catch (Exception e) {
       e.printStackTrace();
@@ -35,7 +47,17 @@ public class Home extends HttpServlet {
     out.close();
   }
 
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  private static Boolean logToDB(String info) {
+    String QUERY = "insert into review_exercise_ values(?)";
+    try (Connection conn = DBConnection.getConnection(); PreparedStatement stmt = conn.prepareStatement(QUERY)) {
+      stmt.setString(1, info);
+      int row = stmt.executeUpdate();
+      if (row > 0) {
+        return true;
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
   }
 }
